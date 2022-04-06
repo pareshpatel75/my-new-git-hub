5ea26b3f61a9bdfb910b7f162ab09c076fe24538
pareshpatel <pareshpatel75@gmail.com>
2022-04-06 09:33:51 +0100

    hi there

---
+++ AvenueLearning.java
@@ -0,0 +1,11 @@
+package MoorgateAvenue;
+
+public class AvenueLearning {
+
+    public static void main(String[] args) {
+
+        System.out.println("hey siri say hello");
+        System.out.println("hey alexa open the door ");
+        
+    }
+}
